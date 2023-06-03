7d8f0365191c9ace20f83cd327b96b7dca60389f
isla <119081489+caizyy@users.noreply.github.com>
2023-06-03 12:05:41 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
 import machine
-Timer = machine.Timer
+from wifihelper import get_signal_strength
 import utime
 import network
-import urequests
-import gc #import garbage collector
-
-led = machine.Pin("LED", machine.Pin.OUT)  # assume the led has not been forcibly removed from the 
+import socket
+import gc
+light = machine.Pin(1, machine.Pin.OUT)
+led = machine.Pin("LED", machine.Pin.OUT)
 led.value(0)
-
 # Connecting to Wi-Fi
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect("idk", "yourpassword")
-
-# Waiting for the Wi-Fi connection to establish
+networks = wlan.scan()
+networks = sorted(networks, key=get_signal_strength)
+for network in networks:
+    ssid = network[0].decode()
+    strength = network[3]
+    if ssid == "": # skip if no name
+        continue
+    print(": 📶", ssid, "\n🎯", strength)
+gc.collect()
+wlan.connect("Netcomm 1946", "Jahivuxoba")
 iteration = 0
 while not wlan.isconnected():
-    print(wlan.isconnected())
     utime.sleep(0.5)
     led.value(0)
     utime.sleep(0.5)
     led.value(1)
     iteration += 1
-    print("Connecting to wifi")
-    if iteration > 20:
+    print("🔄📶")
+    if iteration > 15:
         machine.reset()
-
 # Wi-Fi connection is established
-print("Wi-Fi connected:", wlan.isconnected())
-if wlan.isconnected():
-    led.value(1)
+print("📶", wlan.isconnected())
+led.value(1)
 def timeoutDetect(c):
-    if c:
-        return
-    else:
-        print("Device reset...")
-        machine.reset()
-while wlan.isconnected():
-    iscomplete = False
-    tim = Timer(period=10000, mode=Timer.ONE_SHOT, callback=lambda t:timeoutDetect(iscomplete))
-    r = urequests.request("GET", "http://194.195.250.210/?memory=" + str(gc.mem_alloc()))
-    if r.text == "on":
-        led.value(1)
-    else:
-        led.value(0)
-    iscomplete = True
-    print(r.status_code)
-    print("Memory in use: ", gc.mem_alloc(), "bytes")
-    r.close()
-    gc.collect()
-#when wlan is no longer connected
+    if not c:
+        print("!⌛️")#timeout
+        work()
+def work():
+    while wlan.isconnected():
+        iscomplete = False
+        try:
+            addr = socket.getaddrinfo("192.168.1.22", 3333)[0][-1]
+            s = socket.socket()
+            s.settimeout(10)  # Set the timeout to 10 seconds
+            s.connect(addr)
+            s.sendall(b"GET / HTTP/1.0\r\n\r\n")
+            response = s.recv(4096)
+            s.close()
+            iscomplete = True
+            value = response.decode().split("\r\n")[-1]
+            print(value)
+            light.value(int(value))
+            print("💾: ", gc.mem_alloc(), "bytes")
+        except OSError as e:
+            print("⚠: ", e)
+            timeoutDetect(iscomplete)
+
+        gc.collect()
+
+work()
+# When wlan is no longer connected
 machine.reset()
+
 
